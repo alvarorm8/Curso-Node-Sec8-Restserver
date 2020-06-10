aea418873b4c6e7f5d14e4cef70e160c1e81f704
Alvaro <alvarorm208@gmail.com>
2020-06-10 08:24:45 +0200

    Modificación para que no se vean las credenciales de acceso a la base de datos de Mongo Atlas

--- server/config/config.js
+++ server/config/config.js
@@ -24,7 +24,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://adminAtlas:PvAPjEcJcrKpb1Ki@cluster0-curso-node-ogwkt.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
