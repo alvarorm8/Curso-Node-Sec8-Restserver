2993570b4f1593842284b54983eefdcd17aaf851
Alvaro <alvarorm208@gmail.com>
2020-06-10 08:37:36 +0200

    Adición de comentarios

--- server/config/config.js
+++ server/config/config.js
@@ -19,6 +19,21 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // Base de datos
 // ==============
 
+/*Si subimos a github un código que tiene unas credenciales y no queremos que se vean puedo hacer
+en keroku variables de configuración personalizadas. Puedo ver las variables que tengo con:
+heroku config
+
+Para añadir variables: heroku config:set nombre="Alvaro"
+Para obtener una variable: heroku config:get nombre
+Para borrar una variable: heroku config:unset nombre
+
+De esta manera si yo creo una variable de configuración para cada credencial, en el código puedo
+reemplazar las credenciales por las variables de heroku. Por ejemplo, en la sección 9 del curso:
+
+heroku config:set MONGO_URI="mongodb+srv://adminAtlas:PvAPjEcJcrKpb1Ki@cluster0-curso-node-ogwkt.mongodb.net/cafe"
+
+Tras esto, en el código se pone: process.env.MONGO_URI*/
+
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
